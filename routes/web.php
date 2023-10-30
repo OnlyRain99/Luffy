8b1b6c5ad6e1a3afe571a53247fd1af7b613e35f
OnlyRain99 <105959339+OnlyRain99@users.noreply.github.com>
2023-10-30 18:38:50 +0800

    edit

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::prefix('profile')->group(function(){
     Route::get('/view', [ProfileController::class, 'ProfileView'])->name('profile.view');
     Route::get('/edit', [ProfileController::class, 'ProfileEdit'])->name('profile.edit');
     Route::post('/store',[ProfileController::class, 'ProfileStore'])->name('profile.store');
-    Route::get('/password/view',[ProfileController::class, 'PasswordView'])->name('profile.store');
+    Route::get('/password/view',[ProfileController::class, 'PasswordView'])->name('password.view');
     
     
 });
