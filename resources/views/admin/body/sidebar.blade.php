22666c75ed36f1f72f9781b85921991781659ef9
OnlyRain99 <105959339+OnlyRain99@users.noreply.github.com>
2023-10-30 17:50:20 +0800

    ups

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -72,7 +72,7 @@
           </a>
           <ul class="treeview-menu">
         <li><a href="{{ route('profile.view') }}"><i class="ti-more"></i>Your Profile</a></li>
-        <li><a href=""><i class="ti-more"></i>Change Password</a></li>
+        <li><a href="{{ route('password.view') }}"><i class="ti-more"></i>Change Password</a></li>
             
           </ul>
         </li>
