84a49bf9abff45c2998b2e0c90770d28864e641d
OnlyRain99 <105959339+OnlyRain99@users.noreply.github.com>
2023-11-02 16:21:47 +0800

    group

---
+++ app/Http/Controllers/Backend/Setup/StudentGroupController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Setup;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\StudentGroup;
+
+class StudentGroupController extends Controller
+{
+    public function ViewGroup(){
+        $data['allData'] = StudentGroup::all();
+        return view('backend.setup.group.view_group',$data);
+    }
+}
